9125050dc6ccf35d7a25f22e6b613cf2ed57367c
Deco <deco.vue@gmail.com>
2025-01-10 11:27:07 +0500

    first

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['@/assets/scss/main.scss'],
+  modules: ['@nuxt/ui'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
